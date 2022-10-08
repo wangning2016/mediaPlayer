9be6feab1b08d4f0e1cc8156acfde7539c2c1d8a
wangning2016 <244199475@qq.com>
2022-10-08 17:18:49 +0800

    test 2

--- mainwindow.h
+++ mainwindow.h
@@ -1,3 +1,11 @@
+/*
+ * @Author: wangning2016 244199475@qq.com
+ * @Date: 2022-10-08 17:17:40
+ * @LastEditors: wangning2016 244199475@qq.com
+ * @LastEditTime: 2022-10-08 17:18:42
+ * @FilePath: /mediaPlayer/mainwindow.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
@@ -14,6 +22,7 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
 
 private slots:
     void on_btSelectFile_clicked();
